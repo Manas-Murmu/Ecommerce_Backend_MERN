c94fc30b6575f00224f53577e479ac9e70710ba8
Manas Ranjan Murmu <manasranjanmurmu@gmail.com>
2022-11-28 18:39:32 +0530

    Added User Modal Part1 & UTILS

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,9 +1,33 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is Required"],
-    maxLength: [50, "Name must be less than 50 Chars"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required"],
+      maxLength: [50, "Name must be less than 50 Chars"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+      minLength: [8, "Password shoud be atleast 8 Character"],
+      select: false,
+    },
+    roles: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
   },
-});
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
