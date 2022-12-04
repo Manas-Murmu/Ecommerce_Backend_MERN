5661a08806b27c039da0f28d092a3406d8b8ce56
Manas Ranjan Murmu <manasranjanmurmu@gmail.com>
2022-12-04 10:37:02 +0530

    Scehma forproduct

---
+++ models/product.schema.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a product name"],
+      trim: true,
+      maxLength: [120, "Product name should be max of 120"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide a product price"],
+      maxLength: [5, "Product price  should not be more than 5 digits"],
+    },
+    description: {
+      type: String,
+      //Use Some form of editor - personal assignment
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Collection",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
