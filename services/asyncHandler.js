17b418af207a73bd12ac14ebaabd993de62cccc9
Manas Ranjan Murmu <manasranjanmurmu@gmail.com>
2022-12-04 11:14:00 +0530

    design methods for asynchandler and error

---
+++ services/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(
+      error.code ||
+        json({
+          success: false,
+          message: error.message,
+        })
+    );
+  }
+};
+
+export default asyncHandler;
